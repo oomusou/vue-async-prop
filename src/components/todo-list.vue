5f86803eb31d7691dae9996eae3cb8f3ed50069e
oomusou <oomusou@gmail.com>
2018-10-23 18:19:16 +0800

    NG : Async data to prop

---
+++ src/components/todo-list.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="todo-list">
+    <input type="text" v-model="input">
+    <button @click="addItem">Add</button>
+    <ul>
+      <li v-for="(todo, index) in todos" @click="finishItem(index)" :key="index">
+          {{ todo.title }}, {{ todo.completed }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+const finishItem = function(index) {
+  this.todos[index].completed = !this.todos[index].completed;
+};
+
+const addItem = function() {
+  const elem = {
+    title: this.input,
+    completed: false,
+  };
+  this.todos = [...this.todos, elem];
+};
+
+const props = [
+  'source',
+];
+
+const data = function() {
+  return {
+    input: '',
+    todos: this.source,
+  };
+};
+
+const methods = {
+  finishItem,
+  addItem,
+};
+
+export default {
+  name: 'todo-list',
+  props,
+  data,
+  methods,
+};
+</script>
+
+<style scoped>
+
+</style>
